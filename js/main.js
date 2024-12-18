ddc67fa886f8fa02cdd215e44d0cddc5a4bb3029
Omar Ahmed <oa72357@gmail.com>
2024-12-18 19:56:50 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let image = document.querySelector(".img");
+let container = document.querySelector(".container");
+
+function phones(phone) {
+  image.src = phone;
+}
+function colors(color) {
+  container.style.backgroundColor = color;
+}
